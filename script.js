383048d992607283d194dcb602061a92ffe081c8
MoSattout <27100473+moloflix@users.noreply.github.com>
2024-03-05 01:49:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -145,9 +145,9 @@ function Popup(data) {
     myWindow.document.write('</head><body>');
     myWindow.document.write(data);
     myWindow.document.write('</body></html>');
-    // Close the window after a delay
-    setTimeout(function() {
+    myWindow.document.close(); // necessary for IE >= 10
+    myWindow.onload = function () { // necessary if the div contain images
         myWindow.print();
         myWindow.close();
-    }, 1000); // Adjust the delay as needed
+    };
 }
