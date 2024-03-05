eb173dc78e8cf10963650fe6dc23030dce4e3c52
MoSattout <27100473+moloflix@users.noreply.github.com>
2024-03-05 01:46:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -137,18 +137,18 @@ function PrintElem() {
 }
 
 function Popup(data) {
-    var myWindow = window.open('', 'Receipt', 'height=400,width=600');
-    myWindow.document.write('<html><head><title>Receipt</title>');
-    myWindow.document.write('<link rel="icon" href="fav.ico">');
-    myWindow.document.write('<link rel="stylesheet" href="./receipt/style.css">');
-    myWindow.document.write('<style type="text/css"> *, html {margin:3;padding:0;} </style>');
-    myWindow.document.write('</head><body>');
-    myWindow.document.write(data);
-    myWindow.document.write('</body></html>');
-    myWindow.document.close(); // necessary for IE >= 10
-    myWindow.onload = function () { // necessary if the div contain images
-        myWindow.focus(); // necessary for IE >= 10
-        myWindow.print();
-        myWindow.close();
-    };
+var myWindow = window.open('', 'Receipt', 'height=400,width=600');
+myWindow.document.write('<html><head><title>Receipt</title>');
+myWindow.document.write('<link rel="icon" href="fav.ico">');
+myWindow.document.write('<link rel="stylesheet" href="./receipt/style.css">');
+myWindow.document.write('<style type="text/css"> *, html {margin:3;padding:0;} </style>');
+myWindow.document.write('</head><body>');
+myWindow.document.write(data);
+myWindow.document.write('</body></html>');
+
+// Close the window after a delay
+setTimeout(function() {
+    myWindow.print();
+    myWindow.close();
+}, 1000); // Adjust the delay as needed
 }
