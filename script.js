e0a94f1f4fb06e21f43df652010b1b9460a95feb
MoSattout <27100473+moloflix@users.noreply.github.com>
2024-03-05 01:48:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -145,5 +145,9 @@ function Popup(data) {
     myWindow.document.write('</head><body>');
     myWindow.document.write(data);
     myWindow.document.write('</body></html>');
-
+    // Close the window after a delay
+    setTimeout(function() {
+        myWindow.print();
+        myWindow.close();
+    }, 1000); // Adjust the delay as needed
 }
